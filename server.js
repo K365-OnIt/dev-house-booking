a8f765909c56be781d8dc363ae6af52ee48bb07b
Karabo Elegant Lentsoe <karabolentsoe875@gmail.com>
2021-02-03 02:53:39 +1200

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
-app = require('express');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const _ = require("lodash");
 
-app.get('/', (request, response) =>{
-    response.send('index.html')
+app.use(cors());
+app.use(express.static("public"));
+
+
+// Routes
+app.get("/", (request, response)=>{ 
+    response.sendFile('./public/views/index.html', {root: __dirname})
+})
+app.get("/booking", (request, response)=>{ 
+    response.sendFile('./public/views/Booking.html', {root: __dirname})
+})
+app.get("/location", (request, response)=>{ 
+    response.sendFile('./public/views/Location.html', {root: __dirname})
+})
+app.get("/pricing", (request, response)=>{ 
+    response.sendFile('./public/views/Pricing.html', {root: __dirname})
+})
+app.get("/sign-up", (request, response)=>{ 
+    response.sendFile('./public/views/Sign-Up.html', {root: __dirname})
 })
 
-app.get('/home', (request, response) =>{
-    response.send('index.html')
+app.use((request, response)=> {
+    response.sendFile('./public/views/404.html', {root: __dirname})
+})
+
+const litsen = app.listen(3000, ()=>{
+
+console.log("litsening to port " + litsen.address().port)
 })
